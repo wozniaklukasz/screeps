7e3e3f4891de004c43d6496a3be17a6339beefb9
Łukasz Woźniak <wozniak.lukasz.g@gmail.com>
2018-01-23 15:39:13 +0100

    tutorial 3

---
+++ console.js
@@ -0,0 +1,16 @@
+/*
+
+Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1',
+    { memory: { role: 'harvester' } } );
+
+Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Upgrader1',
+    { memory: { role: 'upgrader' } } );
+
+Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Builder1',
+    { memory: { role: 'builder' } } );
+
+// 550 energy:
+Game.spawns['Spawn1'].spawnCreep( [WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE],
+    'HarvesterBig',
+    { memory: { role: 'harvester' } } );
+ */
